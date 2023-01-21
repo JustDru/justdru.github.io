723d0392d648f8deef23aa5708f1631d0ba6db4c
Andrew <andrewgrahamhshs@gmail.com>
2023-01-20 21:22:40 -0500

    [Update] Added week-2-part2 - Porting week-2-part2 to live

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,72 @@
+"use strict";
+
 (function(){
 
-    function Start()
-    {
+    function DisplayHomePage(){
+        let AboutUsButton = document.getElementById("AboutUsBtn");
+        AboutUsButton.addEventListener("click", function()
+        {
+            location.href = "about.html"
+        });
+    }
+
+    let MainContent = document.getElementsByTagName("main");
+    let MainParagraph = document.createElement("p");
+
+    MainParagraph.setAttribute("id", "MainParagraph");
+    MainParagraph.setAttribute("class", "mt-3");
+    MainParagraph.textContent = "This is the Main Paragraph"
+
+    MainContent.appendChild(MainParagraph)
+
+    function DisplayProductsPage(){
+        let ProductsButton = document.getElementById("ProductsBtn");
+        ProductsButton.addEventListener("click", function()
+        {
+            location.href = "products.html"
+        });
+    }
+
+    function DisplayServicesPage(){
+        let ServicesButton = document.getElementById("ServicesBtn");
+        ServicesButton.addEventListener("click", function()
+        {
+            location.href = "services.html"
+        })
+    }
+
+    function DisplayAboutUsPage(){
+
+    }
+
+    function DisplayContactPage(){
+        let ContactUsButton = document.getElementById("ContactUsBtn");
+        ContactUsButton.addEventListener("click", function()
+        {
+            location.href = "contact.html"
+        });
+    }
+
+    function Start(){
         console.log("App Started!")
+        switch(document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+            case "Our Products":
+                DisplayProductsPage();
+                break;
+            case "About Us":
+                DisplayAboutUsPage();
+                break;
+            case "Our Services":
+                DisplayServicesPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+        }
     }
     window.addEventListener("load", Start)
 })();
